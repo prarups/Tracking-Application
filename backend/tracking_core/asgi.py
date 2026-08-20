1a584c2fbffb12f27d2bb137b28fc12daf4b074e
prarups <paulpraveen328@gmail.com>
2026-08-20 13:20:36 +0530

    Add CancelledErrorMiddleware to handle client disconnects gracefully in ASGI

--- backend/tracking_core/asgi.py
+++ backend/tracking_core/asgi.py
@@ -1,4 +1,5 @@
 import os
+import asyncio
 import django
 
 os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'tracking_core.settings')
@@ -11,8 +12,22 @@ import tracking_core.routing
 
 django_asgi_app = get_asgi_application()
 
+class CancelledErrorMiddleware:
+    """
+    Middleware to catch asyncio.CancelledError when clients disconnect
+    or close their browser tabs early during HTTP requests.
+    """
+    def __init__(self, inner):
+        self.inner = inner
+
+    async def __call__(self, scope, receive, send):
+        try:
+            await self.inner(scope, receive, send)
+        except (asyncio.CancelledError, GeneratorExit):
+            pass
+
 application = ProtocolTypeRouter({
-    "http": django_asgi_app,
+    "http": CancelledErrorMiddleware(django_asgi_app),
     "websocket": JWTAuthMiddleware(
         URLRouter(
             tracking_core.routing.websocket_urlpatterns
